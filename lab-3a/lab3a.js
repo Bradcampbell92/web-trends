5aff770637adefe43bcf9de629f9910feb6fe317
Brad Campbell <campbellb92@hotmail.com>
2018-05-25 15:03:28 -0400

    Lab 3 Giving up point

--- lab-3a/lab3a.js
+++ lab-3a/lab3a.js
@@ -1,7 +1,58 @@
 "use strict";
 var map;
-var Toronto = { lat: 43, lng: -79.38 };
+var coolLocation = [];
+var mapMarkers = [];
+var MapMarker = /** @class */ (function () {
+    function MapMarker(address) {
+        this.Address = address;
+    }
+    return MapMarker;
+}());
+var Toronto = { lat: 43.6532, lng: -79.3832 };
 var initMapConfig = { center: Toronto, zoom: 8 };
+$.ajax({
+    url: './locations.json',
+    dataType: 'json',
+    success: function (data) {
+        //data is an array of objects in this context
+        for (var _i = 0, data_1 = data; _i < data_1.length; _i++) {
+            var cl = data_1[_i];
+            coolLocation.push(cl);
+        }
+    }
+});
 function initMap() {
     map = new google.maps.Map(document.getElementById("map"), initMapConfig);
+    for (var _i = 0, coolLocation_1 = coolLocation; _i < coolLocation_1.length; _i++) {
+        var cl = coolLocation_1[_i];
+        var newMapMarker = new MapMarker(cl.address);
+        mapMarkers.push(newMapMarker);
+    }
+    var markersIndex = 0;
+    setLatitudeLongitude();
+    function setLatitudeLongitude() {
+        mapMarkers[markersIndex].Coordinates = getLatLng(mapMarkers[markersIndex].Address);
+        console.log(mapMarkers[markersIndex]);
+        markersIndex++;
+    }
+    function getLatLng(address) {
+        var geocoder = new google.maps.Geocoder();
+        geocoder.geocode({ 'address': address }, function (result, status) {
+            if (status == 'OK') {
+                console.log(result[0].geometry.location.lat());
+                console.log(result[0].geometry.location.lng());
+                return { lat: result[0].geometry.location.lat(), lng: result[0].geometry.location.lng() };
+            }
+            else {
+                setInterval(getLatLng(address), 1000);
+            }
+        });
+    }
+    function addMarker(coord) {
+        var newMarker = new google.maps.Marker({
+            position: coord,
+            map: map,
+            title: "A cool place to be"
+        });
+    }
 }
--- lab-3a/lab3a.ts
+++ lab-3a/lab3a.ts
@@ -1,22 +1,91 @@
 let map : any;
+let coolLocation : any[] = [];
+let mapMarkers : MapMarker[] = [];
 
 interface LatLng
 {
     lat: number,
     lng: number
 }
-
-interface GoogleMapsConfig
+class MapMarker
 {
-    center: LatLng,
-    zoom : number
+    Address : string;
+    Coordinates: LatLng;
+
+    public constructor(address : string)
+    {
+        this.Address = address;
+    }
 }
-let Toronto : LatLng = { lat: 43, lng: -79.38 }
-let initMapConfig = { center: Toronto, zoom: 8};
+let Toronto : LatLng = { lat: 43.6532, lng: -79.3832 }
+let initMapConfig = { center: Toronto, zoom: 8}
+
+$.ajax({
+    url: './locations.json',
+    dataType: 'json',
+    success: function(data)
+    {
+        //data is an array of objects in this context
+        for(let cl of data)
+        {
+            coolLocation.push(cl);
+        }
+    }
+});
+
 
 function initMap()
 {
     map = new google.maps.Map(
         document.getElementById("map"), initMapConfig
     );
+
+    for(let cl of coolLocation)
+    {
+        let newMapMarker : MapMarker = new MapMarker(cl.address);
+
+        mapMarkers.push(newMapMarker);
+    }
+
+    let markersIndex : number = 0;
+    setLatitudeLongitude();
+
+    function setLatitudeLongitude() : void
+    {
+        mapMarkers[markersIndex].Coordinates = getLatLng(mapMarkers[markersIndex].Address);
+
+        console.log(mapMarkers[markersIndex]);
+
+        markersIndex++;
+    }
+
+
+    function getLatLng(address : string) : LatLng
+    {
+        let geocoder : object = new google.maps.Geocoder();
+
+        geocoder.geocode({ 'address' : address }, function(result, status){
+            if(status == 'OK')
+            {
+                console.log(result[0].geometry.location.lat());
+                console.log(result[0].geometry.location.lng());
+
+                return {lat: result[0].geometry.location.lat(), lng: result[0].geometry.location.lng()};
+            }
+            else
+            {
+                setInterval( getLatLng(address), 1000);
+            }
+        });
+    }
+    function addMarker(coord: LatLng) : void
+    {
+        let newMarker = new google.maps.Marker({
+            position: coord,
+            map: map,
+            title: `A cool place to be`
+        });
+
+    }
+
 }
